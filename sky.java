428a43d007e4de280c4b486adb1b3f8e231944c5
Manishsharmagoku <140081299+Manishsharmagoku@users.noreply.github.com>
2023-07-27 11:13:19 +0530

    Add files via upload

---
+++ sky.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+class GFG {
+    
+    
+	public static void main (String[] args) {
+
+System.out.println("hello world");
+System.out.println("New in the sky-war ");
+	 
+	}
+}
